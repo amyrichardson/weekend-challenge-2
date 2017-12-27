8a26fc14b4c7ba4500d538ed44108eb0e40d6703
Amy Richardson <amyrichardson96@gmail.com>
2017-12-27 07:30:50 -0600

    results appended onto DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
   console.log('jq');
   $('.mathButton').on('click', getInput);
   $('#reset').on('click', resetOperation);
-
+  getResults();
 }); //end ready function
 
 function getInput() {
@@ -20,6 +20,9 @@ function getInput() {
   let numOne = $('#numOne').val();
   let numTwo = $('#numTwo').val();
   let operation = '';
+  $('#numOne').val('');
+  $('#numTwo').val('');
+  $('ul').empty();
   if($(this).val() == "addition") {
     operation = 'addition';
   } else if ($(this).val() == "subtraction"){
@@ -30,27 +33,47 @@ function getInput() {
     operation = 'division';
   } //end else
   let objectToSend = new Operation (numOne, numTwo, operation);
-  console.log(objectToSend);
   $.ajax({
     method: "POST",
     url: "/operation",
     data: objectToSend,
     success: function(response){
-      console.log('back from server!: ' + response);
-      displayResults();
+      getResults();
     }
   });
 } //end getInput
 
-function displayResults() {
+function getResults() {
   $.ajax({
     method: "GET",
     url: "/calculation",
     success: function(response){
-      console.log(response);
-    }
-  });
-}
+    displayResults(response);
+  } //end success
+}); //end get request
+} //end displayResults
+
+function displayResults(resultsArray) {
+    for (let i = 0; i < resultsArray.length; i++) {
+    let operation = resultsArray[i].operation;
+    let numOne = resultsArray[i].numOne;
+    let numTwo = resultsArray[i].numTwo;
+    let result = resultsArray[i].result;
+    if(operation == 'addition'){
+      $('ul').append('<li>' + numOne + ' + ' + numTwo + ' = ' + result);
+    } else if(operation == 'subtraction') {
+      $('ul').append('<li>' + numOne + ' - ' + numTwo + ' = ' + result);
+    } else if(operation == 'multiplication') {
+      $('ul').append('<li>' + numOne + ' * ' + numTwo + ' = ' + result);
+    } else {
+      $('ul').append('<li>' + numOne + ' / ' + numTwo + ' = ' + result);
+    } //end else
+  } //end for loop
+} //end displayResults
+
+
+
+
 
 function resetOperation(){
   console.log('reset button was clicked!');
